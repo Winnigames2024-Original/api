bc37885e116960338626f4d2ba835ca1322d0993
link-short <dora.family228@gmail.com>
2026-01-04 16:05:56 +0500

    Create index.js

---
+++ captcha/index.js
@@ -0,0 +1,68 @@
+const ulhasd = new URLSearchParams(window.location.search);
+const user_api = ulhasd.get('key');
+const captcha_text = ulhasd.get('text');
+const working_api = "KC3B-E4GT-NR5G-TH6D";
+
+if (user_api == working_api) {
+  const generated_imageUrl = "https://google.com/" + captcha_text;
+  window.location.href = generated_imageUrl;
+}
+
+if (type == "cookieDialog") {
+  const fileName = urlParams.get('file');
+  const visibleAlert = urlParams.get('alert');
+  if (fileName == "winnicookies.cookie") {
+    if (visibleAlert == "true") {
+      alert("Cookie message visible now");
+    }
+    else {
+      console.log("Cookie message visible now");
+      console.log(type);
+      console.log(fileName);
+    }
+    console.log("Cookie Visible Now!!!");
+    console.log(type);
+    console.log(fileName);
+  }
+}
+
+
+// JavaScript for cookie management
+        function setCookie(name, value, days) {
+            let expires = "";
+            if (days) {
+                let date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        }
+
+        function getCookie(name) {
+            let nameEQ = name + "=";
+            let ca = document.cookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+            }
+            return null;
+        }
+
+        function acceptCookies() {
+            setCookie('cookieAccepted', 'true', 30); // Set cookie for 30 days
+            document.getElementById('cookieConsent').style.display = 'none';
+        }
+
+        function rejectCookies() {
+            alert("You have rejected cookies. Some functionalities might be limited.");
+            document.getElementById('cookieConsent').style.display = 'none';
+            // You might implement further actions here, like limiting certain features
+        }
+
+        // Check if cookie consent has been given on page load
+        window.onload = function() {
+            if (!getCookie('cookieAccepted')) {
+                document.getElementById('cookieConsent').style.display = 'block';
+            }
+        };
